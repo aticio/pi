85ba38fe23a7f79228e16a6be6e1ef980870f0ae
aticio <aticiozgur@gmail.com>
2022-01-03 17:04:16 +0300

    start app bug fix

--- appstart.sh
+++ appstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /opt/pi/
 pip install -r requirements.txt -U
+kill -9 $(ps -ef | grep "python3 /opt/pi/pi.py" | grep -v grep | awk '{print $2}')
 BINANCE_API_KEY=$1
 BINANCE_API_SECRET=$2
 export BINANCE_API_KEY
